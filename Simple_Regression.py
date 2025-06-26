823a6386b3913e041e3f5cf584f0a987522cf69c
Sudeep Kumar <140135072+SuDeEpKuMaR912@users.noreply.github.com>
2025-06-26 12:07:49 +0530

    Update and rename linear_regression.py to Simple_Regression.py

--- Simple_Regression.py
+++ Simple_Regression.py
@@ -25,10 +25,15 @@ model.fit(diabetes_X_train,diabetes_y_train)
 
 diabetes_y_predicted=model.predict(diabetes_X_test)
 
+print("\n\nResults for SIMPLE LINEAR REGRESSION\n")
 print("Mean squared error is: ", mean_squared_error(diabetes_y_test, diabetes_y_predicted))
 print("Weights: ", model.coef_)
-print("Intercept: ", model.intercept_)
+print("Intercept: ", model.intercept_, end="\n\n")
 
-plt.scatter(diabetes_X_test,diabetes_y_test)
-plt.plot(diabetes_X_test, diabetes_y_predicted)
+plt.scatter(diabetes_X_test,diabetes_y_test, color='blue', label="Actual Test Values")
+plt.plot(diabetes_X_test, diabetes_y_predicted, color='red', label="Predicted values/Regression Line/Best fit line")
+plt.xlabel("Frame")
+plt.ylabel("Target")
+plt.title("Using Diabetes dataset")
+plt.legend(loc="upper left")
 plt.show()
